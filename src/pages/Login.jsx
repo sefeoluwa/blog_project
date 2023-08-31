ee7229947016878be6364f124f836b8429f1b316
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-31 22:36:39 +0100

    user can now login with firebase signInWithPopup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+/* eslint-disable react/prop-types */
+import { auth, provider } from '../firebase-config';
+import { signInWithPopup } from 'firebase/auth';
 
-function Login() {
+function Login({ setIsAuth }) {
+
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, provider).then((result) => {
+      localStorage.setItem("isAuth", true);
+      setIsAuth(true)
+    })
+  }
+
   return (
-    <div>Login</div>
+    <div className='loginPage'>
+      <p>Sign in with Google</p>
+      <button className='login-with-google-btn' onClick={signInWithGoogle}>Sign in with Google</button>
+    </div>
   )
 }
 
