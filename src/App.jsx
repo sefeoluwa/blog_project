bce9c9e6d1851dcbcdda8f23c0fe52e9bea51bb3
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-31 20:03:55 +0100

    added nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+import { Home, CreatePost, Login } from './pages'
+
 
 
 function App() {
 
   return (
-    <>
-      App
-    </>
+    <Router>
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/createpost'>Create Post</ Link>
+        <Link to='/login'>Login</Link>
+      </nav>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/createpost" element={<CreatePost />} />
+          <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   )
 }
 
