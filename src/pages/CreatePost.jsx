5f7b9c0d00d20cf0179c3f9bd18884bd82d01e39
sefeoluwa <sefeoluwa01@gmail.com>
2023-09-01 14:04:30 +0100

    add postCollectionRef from firestore

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import { useState } from "react"
+import { addDoc, collection } from 'firebase/firestore'
+import { db, auth } from '../firebase-config'
 
 function CreatePost() {
+
+  const [title, setTitle] = useState("");
+  const [postText, setPostText] = useState("")
+
+  const postCollectionRef = collection(db, "posts")
+  const createPost = async () => {
+    await addDoc(postCollectionRef, { title, postText, author: {name: auth.currentUser.displayName, id: auth.currentUser.uid} })
+  }
+
   return (
     <div className='createPostPage'>
 
@@ -8,11 +19,19 @@ function CreatePost() {
       <h1>Create New Post</h1>
       <div className="inputGp">
         <label htmlFor="">Title:</label>
-        <input type="text"  placeholder='Title...'/>
+        <input type="text"  placeholder='Title...'
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
+        />
       </div>
       <div className="inputGp">
         <label htmlFor="">Post:</label>
-        <textarea name="" placeholder='Post...' id="" cols="30" rows="10"/>
+        <textarea name="" placeholder='Post...' id="" cols="30" rows="10" 
+        onChange={(e) => {
+          setPostText(e.target.value);
+        }}
+        />
       </div>
       <button>Submit Post</button>
     </div>
