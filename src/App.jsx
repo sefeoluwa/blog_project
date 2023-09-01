c4679164b2c9bbe1a0e1a23985c8d1180d5e6f29
sefeoluwa <sefeoluwa01@gmail.com>
2023-09-01 13:37:58 +0100

    added signout button that redirects to login page :fire:

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
 import { Home, CreatePost, Login } from './pages'
 import { useState } from "react"
+import { signOut } from "firebase/auth";
+import { auth } from "./firebase-config";
 
 
 function App() {
   const [isAuth, setIsAuth] = useState(false);
+
+
+
+  const signUserOut = () => {
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setIsAuth(false);
+     window.location.pathname = '/login'
+    })
+  }
 
   return (
     <Router>
       <nav>
         <Link to='/'>Home</Link>
         <Link to='/createpost'>Create Post</ Link>
-        <Link to='/login'>Login</Link>
+       {!isAuth ? <Link to='/login'>Login</Link> : <button onClick={signUserOut}>Sign Out</button> }
       </nav>
       <Routes>
           <Route path="/" element={<Home />} />
