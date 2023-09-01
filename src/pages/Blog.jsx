1e7395f3825e287d3f8c0d7ccb9baf7116cc6173
sefeoluwa <sefeoluwa01@gmail.com>
2023-09-01 15:42:45 +0100

    individual post can be removed with tash buTton :tada:

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+/* eslint-disable react/jsx-key */
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react'
+import { getDocs, collection, deleteDoc, doc } from 'firebase/firestore'
+import { db } from '../firebase-config'
 
 function Blog() {
+
+  const [postLists, setPostList] = useState([])
+
+  const postCollectionRef = collection(db, "posts")
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postCollectionRef)
+      setPostList(data.docs.map((doc) => ({...doc.data(), id:doc.id })))
+    }
+    getPosts();
+  })
+
+  const deletePost = async (id) => {
+    const postDoc = doc(db, "posts", id)
+    await deleteDoc(postDoc)
+  }
+
   return (
-    <div>Blog</div>
-  )
+    <div className='homePage'>
+      {postLists.map((post) => {
+        return (
+          <div className="post" key={post.id}> 
+            <div className="postHeader">
+              <div className="title">
+                <h1>{post.title}</h1>
+              </div>
+              <div className="deletePost">
+                <button onClick={() => {deletePost(post.id)} }> &#128465; </button>
+              </div>
+            </div>
+            <div className="postTextContainer">{post.postText}</div>
+            <h3>@{post.author?.name}</h3>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Blog
