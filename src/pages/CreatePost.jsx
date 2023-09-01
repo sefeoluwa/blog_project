e1bdaa14bd14dc929285e24c313ce878bbbb6cc8
sefeoluwa <sefeoluwa01@gmail.com>
2023-09-01 13:49:26 +0100

    input boxes and button for createPostPage

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 function CreatePost() {
   return (
-    <div>CreatePost</div>
+    <div className='createPostPage'>
+
+    <div className="cpContainer">
+      <h1>Create New Post</h1>
+      <div className="inputGp">
+        <label htmlFor="">Title:</label>
+        <input type="text"  placeholder='Title...'/>
+      </div>
+      <div className="inputGp">
+        <label htmlFor="">Post:</label>
+        <textarea name="" placeholder='Post...' id="" cols="30" rows="10"/>
+      </div>
+      <button>Submit Post</button>
+    </div>
+    </div>
   )
 }
 
